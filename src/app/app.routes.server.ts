5b01c00aee354b380bbe6da7e25d2635606a96a9
shubh <shubhamknp74@gmail.com>
2026-08-13 10:42:45 +0530

    save krlo

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,9 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { BlogService } from './core/services/blog.service';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'blogs/:slug',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const blogService = new BlogService();
+      return blogService.getAllBlogs().map(blog => ({ slug: blog.slug }));
+    }
   },
   {
     path: '**',
